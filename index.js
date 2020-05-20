a18c9f7972c4274f0f3a22cc7e81d8da6a89fd86
Bryan Neva <bneva@pivotal.io>
2020-05-20 14:03:27 -0400

    Add copy area

--- index.js
+++ index.js
@@ -2,118 +2,150 @@ const names = [];
 
 
 class Names {
-	names = [];
-	observers = [];
+    names = [];
+    observers = [];
 
-	constructor(names) {
-		this.names = names || [];
-	}
+    constructor(names) {
+        this.names = names || [];
+    }
 
-	add(name) {
-		this.names.push(name);
-		this.updateListeners();
-	}
+    add(name) {
+        this.names.push(name);
+        this.updateListeners();
+    }
 
-	remove(i) {
-		this.names.splice(i,1);
-		this.updateListeners(this.names);
-	}
+    remove(i) {
+        this.names.splice(i, 1);
+        this.updateListeners(this.names);
+    }
 
-	addObserver(observer) {
-		this.observers.push(observer);
-		if(this.names && this.names.length !== 0) {
-			this.updateListeners();			
-		}
-	}
+    addObserver(observer) {
+        this.observers.push(observer);
+        if (this.names && this.names.length !== 0) {
+            this.updateListeners();
+        }
+    }
 
-	updateListeners() {
-		this.observers.forEach((obs) => {
-			obs.update(this.names)
-		});
-	}
+    updateListeners() {
+        this.observers.forEach((obs) => {
+            obs.update(this.names)
+        });
+    }
 
-	shuffle() {
-		for (let i = this.names.length - 1; i > 0; i--) {
-        	const j = Math.floor(Math.random() * (i + 1));
-        	[this.names[i], this.names[j]] = [this.names[j], this.names[i]];
-    	}
-    	
-    	this.updateListeners();
-	}
+    shuffle() {
+        for (let i = this.names.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.names[i], this.names[j]] = [this.names[j], this.names[i]];
+        }
+
+        this.updateListeners();
+    }
 }
 
 class NameListElement {
-	el;
-	names;
+    el;
+    names;
 
-	constructor(names) {
-		this.names = names;
-		this.el = document.getElementById('name-list');
-	}
+    constructor(names) {
+        this.names = names;
+        this.el = 'name-list';
+    }
 
-	update(names) {
-		const nameHtml = this.names.names.map((name, i) => `<li>${name} (<button class="remove-button" data-name="${name}" data-index="${i}">remove</button>)</li>`).join('');
-		this.el.innerHTML = nameHtml;
-		this.addRemoveListeners();
-	}
+    reselectElement() {
+        return document.getElementById(this.el);
+    }
 
-	onClickRemove(e) {
-		this.names.remove(e.target.dataset['index']);
-	}
+    update(names) {
+        const nameHtml = this.names.names.map((name, i) => `<li>${name} (<button class="remove-button" data-name="${name}" data-index="${i}">remove</button>)</li>`).join('');
+        this.reselectElement().innerHTML = nameHtml;
+        this.addRemoveListeners();
+    }
 
-	addRemoveListeners() {
-		const removeButtons = document.getElementsByClassName('remove-button');
-		for(let btn of removeButtons) {
-			btn.addEventListener('click', this.onClickRemove.bind(this));
-		}
-	}
+    onClickRemove(e) {
+        this.names.remove(e.target.dataset['index']);
+    }
+
+    addRemoveListeners() {
+        const removeButtons = document.getElementsByClassName('remove-button');
+        for (let btn of removeButtons) {
+            btn.addEventListener('click', this.onClickRemove.bind(this));
+        }
+    }
+}
+
+class CopyAreaElement {
+    el;
+    names;
+
+    constructor(names) {
+        this.names = names;
+        this.el = 'copy-area'
+    }
+
+    reselectElement() {
+        return document.getElementById(this.el);
+    }
+
+    update(names) {
+        const formattedNames = this.names.names.map((name, i) => `${name}\n`).join('');
+        this.reselectElement().value = formattedNames;
+    }
+
+    copy() {
+        this.reselectElement().select();
+        document.execCommand('copy');
+    }
 }
 
 class UrlParams {
-	update(names) {
-		const commaNames = names.join(',');
-		window.history.pushState({names},'',`?names=${commaNames}`);
-	}
+    update(names) {
+        const commaNames = names.join(',');
+        window.history.pushState({names}, '', `?names=${commaNames}`);
+    }
 
-	toArray() {
-		const urlSearchParams = new URLSearchParams(window.location.search)
-		let names;
-		for (let [key, value] of urlSearchParams) {
-			if(key === 'names') {
-				names = value.split(',');
-			}
-		}
+    toArray() {
+        const urlSearchParams = new URLSearchParams(window.location.search)
+        let names;
+        for (let [key, value] of urlSearchParams) {
+            if (key === 'names') {
+                names = value.split(',');
+            }
+        }
 
-		return names;
-	}
+        return names;
+    }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new UrlParams();
+    const names = new Names(urlParams.toArray());
+    const nameList = new NameListElement(names);
+    const copyArea = new CopyAreaElement(names);
+    names.addObserver(nameList);
+    names.addObserver(urlParams);
+    names.addObserver(copyArea)
 
+    const nameForm = document.getElementById('name-form');
+    nameForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const nameField = document.getElementById('name');
+        const value = nameField.value;
+        if (value !== '') {
+            nameField.value = '';
+            nameField.focus();
+            names.add(value);
+        }
+    });
 
-document.addEventListener("DOMContentLoaded", function() { 
-	const urlParams = new UrlParams();
-	const names = new Names(urlParams.toArray());
-	const nameList = new NameListElement(names);
-  	names.addObserver(nameList);
-  	
-  	names.addObserver(urlParams);
+    const shuffleButton = document.getElementById('shuffle-button');
+    shuffleButton.addEventListener('click', () => {
+        names.shuffle();
+    });
 
-  	const nameForm = document.getElementById('name-form');
-  	nameForm.addEventListener('submit', function(e) {
-  		e.preventDefault();
-  		const nameField = document.getElementById('name');
-  		const value = nameField.value;
-  		if(value !== '') {
-	  		nameField.value = '';
-	  		nameField.focus();	
-	  		names.add(value);
-  		}
-  	});
+    names.shuffle();
 
-  	const shuffleButton = document.getElementById('shuffle-button');
-  	shuffleButton.addEventListener('click', () => {
-  		names.shuffle();
-  	});
-
-  	names.shuffle();
+    const copyButton = document.getElementById('copy-button');
+    copyButton.addEventListener('click', () => {
+       copyArea.copy();
+    });
 });
